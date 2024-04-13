bcd04811f8c768ca271f2b7b689d05f7d96b676f
ApexSolutionsNagpur <135802825+ApexSolutionsNagpur@users.noreply.github.com>
2024-04-13 14:22:48 +0530

    first update

---
+++ PatientManagementSoftware/Controllers/InvoicingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PatientManagementSoftware.Controllers
+{
+    public class InvoicingController : Controller
+    {
+        // GET: Invoicing
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
